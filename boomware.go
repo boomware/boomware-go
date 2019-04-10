652d34d8f1388d43926914ed8496437a6e45d1b0
Sergey P <serji.popov@gmail.com>
2019-04-10 12:31:09 +0300

    Added links to docs

--- boomware.go
+++ boomware.go
@@ -2,13 +2,16 @@ package boomware
 
 type Boomware interface {
 	// Sending short text message
+	// For more info https://boomware.com/docs/en/#SMS
 	SMS(r *SMSRequest) *Response
 
 	// Number verification api
+	// More info https://boomware.com/docs/en/#Verification
 	Verify(r *VerifyRequest) *Response
 	VerifyCheck(r *VerifyCheckRequest) *VerifyCheckResponse
 	VerifyInfo(r *VerifyInfoRequest) *VerifyInfoResponse
 
 	// Insight
+	// More info https://boomware.com/docs/en/#Insight
 	Insight(r *InsightRequest) *InsightResponse
 }
