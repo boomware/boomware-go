a0856482d5e14f96bab7d24cc9831d659d712971
Sergey P <serji.popov@gmail.com>
2019-04-29 10:05:32 +0300

    Escape number in query

--- client.go
+++ client.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"strings"
 	"time"
 )
@@ -95,8 +96,8 @@ func (b *boomware) VerifyInfo(r *VerifyInfoRequest) *VerifyInfoResponse {
 
 func (b *boomware) Insight(r *InsightRequest) *InsightResponse {
 	response := new(InsightResponse)
-	urn := fmt.Sprintf("/v1/insight/hlr?number=%s", r.Number)
-	err := b.request(http.MethodGet, urn, nil, response)
+	path := fmt.Sprintf("/v1/insight/hlr?number=%s", url.QueryEscape(r.Number))
+	err := b.request(http.MethodGet, path, nil, response)
 	if err != nil {
 		response.err = err
 	}
@@ -114,9 +115,9 @@ func (b *boomware) MessagingPush(r *MessagingPushRequest) *Response {
 
 // private
 
-func (b *boomware) request(method, urn string, request interface{}, response interface{}) Error {
+func (b *boomware) request(method, path string, request interface{}, response interface{}) Error {
 
-	url := b.endpoint + urn
+	urlString := b.endpoint + path
 
 	var buf io.Reader
 
@@ -124,14 +125,14 @@ func (b *boomware) request(method, urn string, request interface{}, response int
 	if request != nil {
 		jsonBytes, err := json.Marshal(request)
 		if err != nil {
-			return NewError(MarshalRequestErrorCode, fmt.Sprintf("failed to marshal request:%s error:%s", urn, err.Error()))
+			return NewError(MarshalRequestErrorCode, fmt.Sprintf("failed to marshal request:%s error:%s", path, err.Error()))
 		}
 		buf = bytes.NewBuffer(jsonBytes)
 	}
 
-	req, err := http.NewRequest(method, url, buf)
+	req, err := http.NewRequest(method, urlString, buf)
 	if err != nil {
-		return NewError(CreateRequestErrorCode, fmt.Sprintf("failed to create http request:%s error:%s", urn, err.Error()))
+		return NewError(CreateRequestErrorCode, fmt.Sprintf("failed to create http request:%s error:%s", path, err.Error()))
 	}
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("User-Agent", "Boomware/1.0")
@@ -139,7 +140,7 @@ func (b *boomware) request(method, urn string, request interface{}, response int
 
 	resp, err := b.httpClient.Do(req)
 	if err != nil {
-		return NewError(DoRequestErrorCode, fmt.Sprintf("http request:%s error %s", urn, err.Error()))
+		return NewError(DoRequestErrorCode, fmt.Sprintf("http request:%s error %s", path, err.Error()))
 	}
 	defer func() {
 		_ = resp.Body.Close()
@@ -147,21 +148,21 @@ func (b *boomware) request(method, urn string, request interface{}, response int
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return NewError(ReadBodyErrorCode, fmt.Sprintf("failed to read body request:%s error %s", urn, err.Error()))
+		return NewError(ReadBodyErrorCode, fmt.Sprintf("failed to read body request:%s error %s", path, err.Error()))
 	}
 
 	if b.isCodeSucceeded(resp.StatusCode) == false {
 		apiErr := NewError(UnknownErrorCode, "unknown error")
 		err = json.Unmarshal(body, apiErr)
 		if err != nil {
-			return NewError(UnmarshalErrorCode, fmt.Sprintf("failed to unmarshal request:%s error:%s", urn, err.Error()))
+			return NewError(UnmarshalErrorCode, fmt.Sprintf("failed to unmarshal request:%s error:%s", path, err.Error()))
 		}
 		return apiErr
 	}
 
 	err = json.Unmarshal(body, response)
 	if err != nil {
-		return NewError(UnmarshalErrorCode, fmt.Sprintf("failed to unmarshal request:%s error:%s", urn, err.Error()))
+		return NewError(UnmarshalErrorCode, fmt.Sprintf("failed to unmarshal request:%s error:%s", path, err.Error()))
 	}
 
 	return nil
